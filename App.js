c51bb117e4123502de939565cfd0ea8d6b836b2f
il-dar <themagnacarta@protonmail.com>
2018-02-21 11:46:48 -0500

    fixed amplify

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { navigatorConfig } from './src/screens';
 import { persistor, store } from './src/redux/store';
-import Amplify from 'aws-amplify-react-native';
 import Amplify from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 
